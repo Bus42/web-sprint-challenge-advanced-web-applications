332fb98bb80ca2c949d5d26b79ceebcedf62ff78
Greg <gbrewton1980@gmail.com>
2021-10-10 13:47:43 -0600

    renders zero articles without error

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,12 +5,25 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
+import { render } from 'react-dom';
+
+const testArticle = {
+    id: 42,
+    headline: "Less than half of Seattle homes have air conditioning. After a deadly heat wave, ‘everybody’ wants it.",
+    createdOn: Date.now(),
+    author:"",
+    image: 134,
+    summary: "Triple-digit temperatures led to a spike in demand across the region.",
+    body: "Inside the attic of a one-story gray house in a Seattle suburb last week, Jeff Bryson gingerly strapped copper piping across the rafters while wearing a white face mask and a headlamp. The temperature was about 110 degrees in the tight space, which was covered in insulation dust. His work was meant to cool the rest of the home."   
+}
 
 test('renders component without errors', ()=> {
+    render(<Article article={testArticle} />)
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', ()=> {
+    render(<Article article={testArticle} />)
+});
 
 // test('renders "Associated Press" when no author is given', ()=> {
 // });
